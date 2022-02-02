5241d1acd912805aee09f0ee37e4cbe646bb0669
BagaudinP <parchiev.bagaudin@mail.ru>
2022-02-02 16:39:10 +0300

    Добавление карточек в корзину. Оживил кнопки

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import Card from "./components/Card";
+import React from "react";
+import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://611bbf4422020a00175a46da.mockapi.io/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems((prev) => [...prev, obj]);
+  };
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
@@ -15,8 +34,16 @@ function App() {
             <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log("123")}
+              onPLus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
-function Header() {
-    return (
-        <header className="d-flex justify-between align-center p-40">
-            <div className="d-flex align-center">
-                <img width={40} height={40} src="/img/logo.png" />
-                <div>
-                    <h3 className="text-uppercase">React Sneakers</h3>
-                    <p className="opacity-5">Магазин лучших кроссовок</p>
-                </div>
-            </div>
-            <ul className="d-flex">
-                <li className="mr-30">
-                    <img width={18} height={18} src="/img/cart.svg" />
-                    <span>1205 руб.</span>
-                </li>
-                <li>
-                    <img width={18} height={18} src="/img/user.svg" />
-                </li>
-            </ul>
-        </header>
-    );
+function Header(props) {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center cu-p">
+        <img width={40} height={40} alt="img" src="/img/logo.png" />
+        <div>
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-30 cu-p">
+          <img width={18} height={18} alt="img" src="/img/cart.svg" />
+          <span>1205 руб.</span>
+        </li>
+        <li className="cu-p">
+          <img width={18} height={18} alt="img" src="/img/user.svg" />
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
